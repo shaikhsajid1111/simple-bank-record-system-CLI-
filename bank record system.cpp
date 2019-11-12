2338b34b05d1a0139d876c2b82df165e46a21ba3
Shaikh Sajid <44086997+shaikhsajid1111@users.noreply.github.com>
2019-11-12 16:22:25 +0530

    replication of data feature
    feature added:
    1) checks if account number already exists
    2) if exist it clears all the data and prompts for data again
    3) addition of search feature, it searches the data in the file and return whether it exists

--- bank record system.cpp
+++ bank record system.cpp
@@ -7,9 +7,14 @@ using namespace std;
 
 class Bank_account{
 public:
-	void enter_data();	
+	bool account_exist(char *search);
+	void enter_data();
+	void search_account_number();	
+
 };
 int length_function(int number);
+
+/*main function*/
 int main(int argc, char const *argv[])
 {
 	Bank_account customer;
@@ -27,12 +32,36 @@ int length_function(int number){
 	return (counter);
 }
 /************************outside function above************************/
+bool Bank_account::account_exist(char *search){
 
+	int offset; 
+    string line;
+    ifstream Myfile;
+    Myfile.open ("data.txt");
+
+    if(Myfile.is_open())
+    {
+        while(!Myfile.eof())
+        {
+            getline(Myfile,line);
+            if ((offset = line.find(search, 0)) != string::npos) 
+            {
+            // cout << "found '" << search << " \n\n"<< line  <<endl;
+             return true;
+            }
+        }
+        Myfile.close();
+    }
+    else
+    cout<<"Unable to open this file."<<endl;
+    return false;
+
+}
 void Bank_account::enter_data(){
 	
 	while (true){
 		string f_name,l_name;
-
+		START:
 		cout << "Enter Account Holder's First Name : "<< endl;
 		cin >> l_name;
 		if(cin.fail()){
@@ -58,6 +87,15 @@ void Bank_account::enter_data(){
 			cin.ignore(100,'\n');
 			goto ACCOUNT;
 		}
+		
+		char search[10];
+		itoa(temp_account,search,10);
+		if(account_exist(search)){
+			cout << "Account already exist!\nHence your entered data will be cleared!";
+			cout << "Enter data \n";
+			goto START;
+		}
+		
 		int length_of_account_number = length_function(temp_account);
 		if(length_of_account_number >= 8 && length_of_account_number <= 10){
 			account_number = temp_account;
@@ -105,5 +143,52 @@ void Bank_account::enter_data(){
 		data_file << "Name : " << f_name +"\t"+ l_name << "\t" << "account number : " << account_number << "\t" << "Account type : " << account_type << "\t" << "Balance :" << balance <<"\t" <<endl;
 		data_file.close();
 		cout << "Data saved successfully!";
+
+}
+}
+void Bank_account::search_account_number(){
+	while(true){
+		unsigned long int account_number,temp;
+		ACCOUNT_NUMBER:
+		cout << "Enter account number: " << endl;
+		cin >> temp;
+		if(cin.fail()){
+			cout << "Invalid Input!" << endl;
+			cin.clear();
+			cin.ignore(100,'\n');
+			goto ACCOUNT_NUMBER;
+		}
+		int length_of_account_number = length_function(temp);
+		if(length_of_account_number >= 8 && length_of_account_number <= 10){
+			account_number = temp;
+		}
+		else{
+			cout << "Account number cannot be of " << length_of_account_number << " digits!" <<endl;
+			goto ACCOUNT_NUMBER;
+		}
+		/*searching*/
+		int offset;
+		string line;
+		ifstream data_file;
+		char search[10];
+		
+		itoa(account_number,search,10);
+		
+		data_file.open("data.txt");
+		if(data_file.is_open()){
+			while(!data_file.eof()){
+				getline(data_file,line);
+				 if((offset = line.find(search, 0)) != string::npos){
+				 	cout << "found!\n";
+				 	cout << line << endl;
+				 }
+				 data_file.close();
+			}
+		}
+		else{
+			cout << "Could not open file!";
+			
+		}
+
 	}
 }
