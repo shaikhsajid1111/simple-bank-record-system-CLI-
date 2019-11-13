a359402a4841bd734bd64e0e47f2da3daa9ba27e
Shaikh Sajid <44086997+shaikhsajid1111@users.noreply.github.com>
2019-11-13 20:28:47 +0530

    major changes
    1) changed all the input types to string
    2) added feature to edit record
    3) search data optimization
    4) added function name replace_str
    and many more

--- bank record system.cpp
+++ bank record system.cpp
@@ -3,18 +3,21 @@
 #include<cstdlib>
 #include<cstring>
 #include<cctype>
+#include<sstream>
 using namespace std;
 
 class Bank_account{
 public:
-	bool account_exist(char *search);
+	bool account_exist(string search);
 	void enter_data();
 	void search_account_number();	
 	void display_record();
+	void edit_rec();
 	void update_data();
+
 };
 int length_function(int number);
-
+string replace_str(string s, string oldVal, const string newVal);
 /*main function*/
 int main(int argc, char const *argv[])
 {
@@ -23,6 +26,7 @@ int main(int argc, char const *argv[])
 	//customer.enter_data();
 	//customer.search_account_number();
 	customer.update_data();
+	
 	system("pause");
 	return 0;
 }
@@ -35,8 +39,38 @@ int length_function(int number){
 	}
 	return (counter);
 }
+string replace_str(string s, string oldVal, const string newVal) // replace oldVal with newVal in s
+{
+	if (!s.empty() && !oldVal.empty()) {
+		auto sbeg = s.begin(), OVbeg = oldVal.begin();
+		unsigned cnt = 0;
+
+		while (sbeg != s.end()) {
+			while (sbeg != s.end() && OVbeg != oldVal.end() && *sbeg == *OVbeg) {
+				++cnt;
+				++sbeg;
+				++OVbeg;
+			}
+			if (cnt == oldVal.size()) {
+				sbeg = s.erase(sbeg - oldVal.size(), sbeg);
+				for (decltype(newVal.size()) ind = 0; ind != newVal.size(); ++ind) {
+					sbeg = s.insert(sbeg, newVal[ind]);
+					++sbeg;
+				}
+			}
+			cnt = 0; // This?
+			OVbeg = oldVal.begin();
+
+			if (sbeg != s.end())
+				++sbeg;
+		}
+	}
+	return s;
+}
+
+
 /************************outside function above************************/
-bool Bank_account::account_exist(char *search){
+bool Bank_account::account_exist(string search){
 
 	int offset; 
     string line;
@@ -79,7 +113,7 @@ void Bank_account::enter_data(){
 		}
 		
 		//account number cannot be negative 
-		unsigned long int account_number,temp_account;	
+		string account_number,temp_account;	
 
 		ACCOUNT:
 		cout << "Enter Account Number for " << f_name + " " + l_name << endl;
@@ -91,16 +125,23 @@ void Bank_account::enter_data(){
 			cin.ignore(100,'\n');
 			goto ACCOUNT;
 		}
+		try{
+			stod(temp_account);
+		}
+		catch(invalid_argument &ex){
+			cout << "Enter numbers only!";
+			goto ACCOUNT;
+		}
 		
-		char search[10];
-		itoa(temp_account,search,10);
-		if(account_exist(search)){
+		//char search[10];
+		//itoa(temp_account,search,10);
+		if(account_exist(temp_account)){
 			cout << "Account already exist!\nHence your entered data will be cleared!";
 			cout << "Enter data \n";
 			goto START;
 		}
 		
-		int length_of_account_number = length_function(temp_account);
+		int length_of_account_number = strlen(temp_account.c_str());
 		if(length_of_account_number >= 8 && length_of_account_number <= 10){
 			account_number = temp_account;
 		}
@@ -129,7 +170,7 @@ void Bank_account::enter_data(){
 			goto ACCOUNT_TYPE;
 		}
 		/*balance*/
-		double balance,temp_balance;
+		string balance,temp_balance;
 		BALANCE:
 		cout << "Enter Balance: \n";
 		cin >> temp_balance;
@@ -139,19 +180,24 @@ void Bank_account::enter_data(){
 			cin.ignore(100,'\n');
 			goto BALANCE;
 		}
-		else{
-			balance = temp_balance;
+		try{
+			stod(temp_balance);
 		}
+		catch(invalid_argument &ex){
+			cout << "Enter numbers only! \n";
+			goto BALANCE;
+		}
+		balance = temp_balance;
 		fstream data_file;
 		data_file.open("data.txt", std::ios_base::app);
-		data_file << "Name : " << f_name +"\t"+ l_name << "\t" << "account number : " << account_number << "\t" << "Account type : " << account_type << "\t" << "Balance :" << balance <<"\t" <<endl;
+		data_file << f_name << "|" << l_name << "|" << account_number << "|" << account_type << "|" << balance << endl;
 		data_file.close();
 		cout << "Data saved successfully!";
 
 }
 }
 void Bank_account::search_account_number(){
-	while(true){
+	
 		unsigned long int account_number,temp;
 		ACCOUNT_NUMBER:
 		cout << "Enter account number: " << endl;
@@ -195,8 +241,6 @@ void Bank_account::search_account_number(){
 		}
 
 	}
-}
-
 void Bank_account::display_record(){
 		ifstream data_file;
 		data_file.open("data.txt");
@@ -222,50 +266,159 @@ void Bank_account::display_record(){
 void Bank_account::update_data(){
 	fstream data_file;
 	data_file.open("data.txt");
-	//if file is not found
 	if(data_file.fail()){
-		cout << "Data doesn't exists!";
+		cout << "Error While opening file";
+
 	}
-	
-	//user input
-	while(true){
-	ACCOUNT:
-	int search_account;
-	cout << "Enter Account Number : "<< endl;
-	cin >> search_account;
+	stringstream file_content;
+	file_content << data_file.rdbuf();
+	string data = file_content.str();
+	data_file.close();
+	//data_file.open("data.txt", ios::out | ios::trunc);
+	/*account number inpus and exception handling*/
+	ACCOUNT_SEARCH:
+	unsigned long int account_number_search;
+	cout << "Enter Account Number to search : \n";
+	cin >> account_number_search;
 	if(cin.fail()){
-		cout << "Invalid Input\n";
+		cout << "Error Occured!\n";
 		cin.clear();
 		cin.ignore(100,'\n');
-		goto ACCOUNT;
+		goto ACCOUNT_SEARCH;
 	}
-	int length_of_account_number = length_function(search_account);
+	int length_of_account_number = length_function(account_number_search);
 	if(length_of_account_number < 8 || length_of_account_number > 10){
-		cout << "Account number cannot be of " << length_of_account_number << " digits!";
-		goto ACCOUNT;
-	}
-	
-	int offset;
-	string line;
-	
-	char search[10];
-	
-	itoa(search_account,search,10);
-	string current_line;
-	
-	
-	while(!data_file.eof()){
-		getline(data_file,line);
-		if((offset = line.find(search, 0)) != string::npos){
-			cout << "found!\n";
-			current_line = line;
-			cout << "the line is " << current_line<<endl; 
-			 }
-			 data_file.close();
+			cout << "Account number cannot be of " << length_of_account_number << endl;
+			goto ACCOUNT_SEARCH;
 		}
-	
-	}
+	//cout << account_number_search;
+	//cout << data;
+	char search[10];
+	itoa(account_number_search,search,10);
+		/*account number inpus and exception handling*/
+		if(account_exist(search)){
+			cout << "Account number exist!";
+		}
 
-	
-	}	
 
+
+		int offset;
+		string line,new_line;
+		//ifstream ;
+
+		data_file.open("data.txt");
+		if(data_file.is_open()){
+			while(!data_file.eof()){
+				getline(data_file,line);
+				 if((offset = line.find(search, 0)) != string::npos){
+				 	cout << line << endl;
+
+					cout << "Enter new Data\n";
+					data_file.close();
+					/**/
+
+		string f_name,l_name;
+		START:
+		cout << "Enter Account Holder's First Name : "<< endl;
+		cin >> l_name;
+		if(cin.fail()){
+			cout << "Invalid Input\n";
+		}
+
+		cout << "Enter Account Holder's Last Name: "<<endl;
+		cin >> f_name;
+		if(cin.fail()){
+			cout << "Invalid Input\n";
+		}
+		
+		//account number cannot be negative 
+		string account_number,temp_account;	
+
+		ACCOUNT:
+		cout << "Enter Account Number for " << f_name + " " + l_name << endl;
+		cin >> temp_account;
+		
+		if(cin.fail()){
+			cout << "Invalid Input\n";
+			cin.clear();
+			cin.ignore(100,'\n');
+			goto ACCOUNT;
+		}
+		try{
+			stod(temp_account);
+		}
+		catch(invalid_argument &ex){
+			cout << "Enter numbers only!";
+			goto ACCOUNT;
+		}
+		
+		//char search[10];
+		//itoa(temp_account,search,10);
+		if(account_exist(temp_account)){
+			cout << "Account already exist!\nHence your entered data will be cleared!";
+			cout << "Enter data \n";
+			goto START;
+		}
+		
+		int length_of_account_number = strlen(temp_account.c_str());
+		if(length_of_account_number >= 8 && length_of_account_number <= 10){
+			account_number = temp_account;
+		}
+		else{
+			cout << "Account Number Cannot be Shorter than 8 digits or greater than 10! \n";
+			goto ACCOUNT;
+		}
+		/*account type*/
+		string account_type,temp_type;
+
+		ACCOUNT_TYPE:
+		cout << "Account type : ('s' for saving and 'c' for current) : \n";
+		cin >> temp_type;
+		if(cin.fail()){
+			cout << "Enter Character!\n";
+			goto ACCOUNT_TYPE;
+		}
+		if(temp_type == "s"){
+			account_type = "saving";
+		}
+		else if(temp_type == "c"){
+			account_type = "current";
+		}
+		else{
+			cout << "Invalid Input!\n";
+			goto ACCOUNT_TYPE;
+		}
+		/*balance*/
+		string balance,temp_balance;
+		BALANCE:
+		cout << "Enter Balance: \n";
+		cin >> temp_balance;
+		if(cin.fail()){
+			cout << "Invalid Input!\n";
+			cin.clear();
+			cin.ignore(100,'\n');
+			goto BALANCE;
+		}
+		try{
+			stod(temp_balance);
+		}
+		catch(invalid_argument &ex){
+			cout << "Enter numbers only! \n";
+			goto BALANCE;
+		}
+		balance = temp_balance;
+		
+	 new_line = f_name + "|" + l_name + "|" + account_number + "|" + account_type + "|" + balance;
+	 string new_data = replace_str(data,line,new_line);
+	 cout << new_data;
+	 data_file.open("data.txt", ios::out | ios::trunc);
+	 data_file << new_data;
+		/**/
+	//cout << new_line;
+				 }
+				
+			}
+
+
+
+	}	}
